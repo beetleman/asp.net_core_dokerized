f8c6129fba7d3c1c25fd3cca8de2ee01926889d6
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-06-05 01:03:05 +0200

    add init sql script

---
+++ entrypoint.mssql.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function importData {
+    sleep 20s;
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P qsddF234fdfFFsx -d master -i setup.sql;
+};
+
+
+importData &
+/opt/mssql/bin/sqlservr;
---
+++ src/sql/setup.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE APP_DB;
+GO
+USE APP_DB;
+GO
+CREATE TABLE Products (ID int, ProductName nvarchar(max));
+GO
